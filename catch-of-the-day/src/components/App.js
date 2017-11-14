8e9ddd0fd32f46bf4c4cc5a65015cbab0c096507
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-14 16:25:29 -0500

    Creates app component

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        <div className="catch-of-the-day">
+            <div className="menu">
+                <Header />
+            </div>
+            <Order />
+            <Inventory />
+        </div>
+    }
+}
