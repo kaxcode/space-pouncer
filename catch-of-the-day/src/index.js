8561979afb492f53b0d5578e028172a3f0e2b9bb
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-16 23:32:08 -0500

    Updates index to use React Router 4v.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { render } from 'react-dom'
+import { BrowserRouter as Router, Route, Miss } from 'react-router-dom'
 import './css/style.css';
 
 //components
 import App from './components/App';
-// import StorePicker from './components/StorePicker';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={StorePicker}/>
+        <Route path="/store/:storeId" component={App}/>
+        <Miss component={NotFound} />
+      </div>
+    </Router>
+  )
+}
 
-render (<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
