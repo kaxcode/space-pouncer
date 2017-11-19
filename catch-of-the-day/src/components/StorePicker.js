8508ede61a442111e4259a461cd2af3084ab9301
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-19 16:47:03 -0500

    Create a gotToStore method withing the StorePicker component class.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the URL');
+        // Get text from input box
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
                 <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
                 <button type="submit">Visit Store -></button>
