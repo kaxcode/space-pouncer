4ccae7077e38390fc751904f7647df09eebde75d
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-20 21:55:27 -0500

    Creates basic form.

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -3,7 +3,17 @@ import React from 'react';
 class AddFishForm extends React.Component {
     render() {
         return(
-            <p>AddFishForm</p>
+            <form className="fish-edit">
+                <input type="text" placeholder="Fish Name" />
+                <input type="text" placeholder="Fish Price" />
+                <select>
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea placeholder="Fish Desc" ></textarea>
+                <input type="text" placeholder="Fish Image" />
+                <button type="submit">+ Add Item</button>
+            </form>
         )
     }
 }
