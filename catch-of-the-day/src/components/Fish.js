88ee9d8e9345bc7c1f7e4588a265814703ebfa67
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-23 23:24:45 -0500

    Adds a return for testing

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 class Fish extends React.Component {
-
+    render() {
+        return (
+            <li className="menu-fish">  
+                Fishy
+            </li>
+        )
+    }
 }
 
 export default Fish;
