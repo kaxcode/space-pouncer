91c609ba43ed16309c0676432d4b8df9759c3756
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-24 23:53:52 -0500

    Adds firebase info to connec tot firebase anywhere in app.

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,5 +1,9 @@
 import Reabase from 're-base';
 
 const base = Rebase.createClass({
-
+    apiKey: "AIzaSyDTjdtpLtsxsxotYpbz7u5BV3VhrI_-cZ8",
+    authDomain: "space-pouncer-kenia.firebaseapp.com",
+    databaseURL: "https://space-pouncer-kenia.firebaseio.com",
 });
+
+export default base;
