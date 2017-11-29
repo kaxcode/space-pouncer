04860b745c9ff2812401eba9cb833a187133c28f
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-28 22:15:10 -0500

    Adds inventory propTypes.

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
+import PropTypes from 'prop-types';
 
 class Inventory extends React.Component {
   constructor() {
@@ -48,5 +49,13 @@ class Inventory extends React.Component {
     )
   }
 }
+
+Inventory.propTypes = {
+  fishes: PropTypes.object.isRequired,
+  updateFish: PropTypes.func.isRequired,
+  removeFish: PropTypes.func.isRequired,
+  addFish: PropTypes.func.isRequired,
+  loadSamples: PropTypes.func.isRequired
+};
 
 export default Inventory;
